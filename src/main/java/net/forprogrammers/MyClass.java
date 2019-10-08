d7344a89c03689ad15ea8d870dab41b4e472ac2f
janusz <janusz.agh@gmail.com>
2019-10-08 18:47:42 +0200

    inheritance

---
+++ src/main/java/net/forprogrammers/MyClass.java
@@ -0,0 +1,25 @@
+package net.forprogrammers;
+
+/**
+ * Created by Janusz Kacki on 08/10/2019. Project; bielmarcus
+ */
+public class MyClass implements IMyClass {
+
+    @Override
+    public IStudent findByName(String name) {
+
+        return null;
+    }
+
+    @Override
+    public IStudent findBySurname(String surname) {
+
+        return null;
+    }
+
+    @Override
+    public int count() {
+
+        return 0;
+    }
+}
