58928f44dca2364b4338718ded38e2035c11c9b3
janusz <janusz.agh@gmail.com>
2019-10-08 13:10:05 +0200

    add Name class

--- src/main/java/pl/janusz/lesson2/Person.java
+++ src/main/java/pl/janusz/lesson2/Person.java
@@ -1,9 +1,13 @@
 package pl.janusz.lesson2;
 
+import pl.janusz.lesson3.Name;
+
 /**
  * Created by Janusz Kacki on 08/10/2019. Project; bielmarcus
  */
 public class Person {
+
+    private Name personName;
 
     public String hello() {
 
