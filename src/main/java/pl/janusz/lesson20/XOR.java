2d645fdd8a1f48eff47fcc740299f060acb4545f
janusz <janusz.agh@gmail.com>
2019-10-15 22:44:52 +0200

    lesson 19 part 4

--- src/main/java/pl/janusz/lesson20/XOR.java
+++ src/main/java/pl/janusz/lesson20/XOR.java
@@ -5,4 +5,22 @@ package pl.janusz.lesson20;
  */
 public class XOR {
 
+    public static String mirrorBinaryNumber(String binary) {
+
+        if (binary == null || "".equals(binary)) {
+            return binary;
+        }
+
+        return recTailReverse(binary, binary.length() - 1, "");
+    }
+
+    private static String recTailReverse(String binary, int position, String acc) {
+
+        if (position < 0) {
+            return acc;
+        } else {
+            return recTailReverse(binary, position - 1, acc + binary.charAt(position));
+        }
+    }
+
 }
