0862beeee06df04464c344a1d93314b84844c1e5
janusz <janusz.agh@gmail.com>
2019-10-18 10:25:56 +0200

    10 digits ISBN

--- src/test/java/pl/janusz/greencroft/isbntools/ValidateISBNTest.java
+++ src/test/java/pl/janusz/greencroft/isbntools/ValidateISBNTest.java
@@ -23,28 +23,64 @@ public class ValidateISBNTest {
     @Test
     public void shouldCheckAnotherValidISBN() {
 
-        int validISBN = 140177396;
+        String validISBN = "0140177396";
 
         boolean result = validator.checkISBN(validISBN);
 
-        assertThat("140177396 should be validated", result, is(true));
+        assertThat("0140177396 should be validated", result, is(true));
     }
 
     @Test
     public void shouldCheckValidISBN() {
 
-        int validISBN = 1404491116;
+        String validISBN = "0306406152";
         boolean result = validator.checkISBN(validISBN);
 
-        assertThat("1404491116 shoould be validated", result, is(true));
+        assertThat("01404491116 shoould be validated", result, is(true));
     }
 
     @Test
     public void shouldNotPassInvalidISBNNumber() {
 
-        int invalidISBN = 1404491117;
+        String invalidISBN = "1234567890";
         boolean result = validator.checkISBN(invalidISBN);
 
-        assertThat("1404491117 should'nt be validated", result, is(false));
+        assertThat("01404491117 should'nt be validated", result, is(false));
+    }
+
+    @Test
+    public void shouldNotValidateNon10digitsISBNsAndThrowExceptions() {
+
+        String invalidISBN = "123456789";
+
+        try {
+            boolean result = validator.checkISBN(invalidISBN);
+            fail();
+        } catch (Exception e) {
+            assertThat(e, instanceOf(NumberFormatException.class));
+        }
+    }
+
+    @Test
+    public void shouldValidateWihXOnTheLastPosition() {
+
+        String validISBN = "012000030X";
+
+        boolean result = validator.checkISBN(validISBN);
+
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void nonDigitStringShouldThrowException() {
+
+        String invalidISBN = "X123456789";
+
+        try {
+            boolean result = validator.checkISBN(invalidISBN);
+            fail();
+        } catch (Exception e) {
+            assertThat(e, instanceOf(IllegalArgumentException.class));
+        }
     }
 }
