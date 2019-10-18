0f004521a561a964aa6dccd21089cfcbe929a8de
janusz <janusz.agh@gmail.com>
2019-10-18 14:46:04 +0200

    stock management stub class

--- src/main/java/pl/janusz/greencroft/isbntools/ExternalISBNDataService.java
+++ src/main/java/pl/janusz/greencroft/isbntools/ExternalISBNDataService.java
@@ -3,6 +3,7 @@ package pl.janusz.greencroft.isbntools;
 /**
  * Created by Janusz Kacki on 18/10/2019. Project; bielmarcus
  */
-public class StockManagement {
+public interface ExternalISBNDataService {
 
+    Book lookup(String isbn);
 }
