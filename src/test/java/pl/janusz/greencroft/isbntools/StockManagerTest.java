078c737cf0cfad078666b0a42a8e839e087fb48f
janusz <janusz.agh@gmail.com>
2019-10-18 18:56:30 +0200

    stock management mock class

--- src/test/java/pl/janusz/greencroft/isbntools/StockManagerTest.java
+++ src/test/java/pl/janusz/greencroft/isbntools/StockManagerTest.java
@@ -2,6 +2,7 @@ package pl.janusz.greencroft.isbntools;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
@@ -16,60 +17,89 @@ public class StockManagerTest {
     private ExternalISBNDataService database;
 
     private String isbn;
+    private String isbnExternal;
     private StockManager stockManager;
     private String validResponse;
+    private String validResponseExternal;
+    private Book book;
+    private Book bookExternal;
+    private ExternalISBNDataService mockDatabase;
+    private ExternalISBNDataService mockWebService;
 
     @Test
     public void shouldUseDatabaseIfDataIsPresent() {
 
-        ExternalISBNDataService mockDatabase = Mockito.mock(ExternalISBNDataService.class);
-        stockManager.setDatabase(mockDatabase);
         String locatorCode = stockManager.getLocatorCode(isbn);
 
-        assertThat(locatorCode, is(equalTo(validResponse)));
+        Mockito
+                .verify(mockDatabase, Mockito.times(1))
+                .lookup(isbn);
+        Mockito
+                .verify(mockWebService, Mockito.times(0))
+                .lookup(isbn);
     }
 
     @Test
     public void shouldUseWebServiceIfDataIsNotPresent() {
 
-        ExternalISBNDataService mockWebService = Mockito.mock(ExternalISBNDataService.class);
+        String locatorCode = stockManager.getLocatorCode(isbnExternal);
+
+        Mockito
+                .verify(mockDatabase, Mockito.times(1))
+                .lookup(isbnExternal);
+
+        Mockito
+                .verify(mockWebService, Mockito.times(1))
+                .lookup(isbnExternal);
     }
 
     @Before
     public void setUp() throws Exception {
 
         isbn = "0140177396";
+        isbnExternal = "1234567890";
         validResponse = "7396J4";
+        validResponseExternal = "7890T1";
 
         stockManager = new StockManager();
+        book = new Book(isbn, "Of Mice and Men", "J. Steinbeck");
+        bookExternal = new Book(isbnExternal, "Algorithms", "T. Cormen");
 
-        webService = new ExternalISBNDataService() {
+        mockDatabase = Mockito.mock(ExternalISBNDataService.class);
+        mockWebService = Mockito.mock(ExternalISBNDataService.class);
 
-            @Override
-            public Book lookup(String isbn) {
+        Mockito
+                .when(mockDatabase.lookup(isbn))
+                .thenReturn(book);
 
-                return new Book(isbn, "Of Mice and Men", "J. Steinbeck");
-            }
-        };
+        Mockito
+                .when(mockDatabase.lookup(isbnExternal))
+                .thenReturn(null);
 
-        database = new ExternalISBNDataService() {
+        Mockito
+                .when(mockWebService.lookup(isbn))
+                .thenReturn(null);
 
-            @Override
-            public Book lookup(String isbn) {
+        Mockito
+                .when(mockWebService.lookup(isbnExternal))
+                .thenReturn(bookExternal);
 
-                return null;
-            }
-        };
-
-//        stockManager.setDatabase(database);
-//        stockManager.setWebService(webService);
+        stockManager.setDatabase(mockDatabase);
+        stockManager.setWebService(mockWebService);
     }
 
     @Test
-    public void shouldgetCorrectLocatorCode() {
+    public void shouldGetCorrectLocatorCode() {
 
         String locatorCode = stockManager.getLocatorCode(isbn);
 
         assertThat(locatorCode, is(equalTo(validResponse)));
+    }
+
+    @Test
+    public void shouldGetAnotherLocatorCode() {
+
+        String locatorCode = stockManager.getLocatorCode(isbnExternal);
+        assertThat(locatorCode, is(equalTo(validResponseExternal)));
     }
 }
