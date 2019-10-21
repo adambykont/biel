62159ab5288f9d60c37d754442cdd08170fb8fc3
janusz <janusz.agh@gmail.com>
2019-10-21 20:11:53 +0200

    Todo App

---
+++ src/main/java/pl/janusz/ut28minutes/TodoService.java
@@ -0,0 +1,8 @@
+package pl.janusz.ut28minutes;
+
+/**
+ * Created by Janusz Kacki on 21/10/2019. Project; bielmarcus
+ */
+public interface TodoService {
+
+}
---
+++ src/test/java/pl/janusz/ut28minutes/TodoBusinessTest.java
@@ -0,0 +1,14 @@
+package pl.janusz.ut28minutes;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Created by Janusz Kacki on 21/10/2019. Project; bielmarcus
+ */
+public class TodoBusinessTest {
+
+}
