349e4a040a58a0780ee144f8d517a843a4597a2b
janusz <janusz.agh@gmail.com>
2019-10-22 18:01:06 +0200

    Spy in Mockito

---
+++ src/test/java/pl/janusz/baeldung/kata/MySpyTest.java
@@ -0,0 +1,14 @@
+package pl.janusz.baeldung.kata;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Created by Janusz Kacki on 22/10/2019. Project; bielmarcus
+ */
+public class MySpyTest {
+
+}
