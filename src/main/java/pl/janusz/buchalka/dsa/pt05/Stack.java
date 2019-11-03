9ca765d1b9275516c7e78b952bb93ad0fef1ca0b
janusz <janusz.agh@gmail.com>
2019-11-03 15:10:15 +0100

    DSA Stacks
    tests

---
+++ src/main/java/pl/janusz/buchalka/dsa/pt05/Stack.java
@@ -0,0 +1,13 @@
+package pl.janusz.buchalka.dsa.pt05;
+
+/**
+ * Created by Janusz Kacki on 03/11/2019. Project; bielmarcus
+ */
+public interface Stack<E> {
+
+    void push(E element);
+
+    E pop();
+
+    E peek();
+}
